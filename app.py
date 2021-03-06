0db8c5bd34296a05fb13b57aa4a103df61b9b3ee
Dean Rumsby <dperumsby@gmail.com>
2021-03-06 17:31:15 +0000

    tidied up animation function

--- app.py
+++ app.py
@@ -9,54 +9,50 @@ clock = pg.time.Clock()
 
 class Dizzy:
 
-    STILL_1 = pg.image.load('assets/dizzy-still-1.png').convert()
-    STILL_2 = pg.image.load('assets/dizzy-still-2.png').convert()
+    STILL_FRAMES = [
+        pg.image.load('assets/dizzy-still-0.png').convert(),
+        pg.image.load('assets/dizzy-still-1.png').convert()
+    ]
 
-    WALKING_L1 = pg.image.load('assets/dizzy-walk-left-1.png').convert()
-    WALKING_L2 = pg.image.load('assets/dizzy-walk-left-2.png').convert()
-    WALKING_L3 = pg.image.load('assets/dizzy-walk-left-3.png').convert()
-    WALKING_L4 = pg.image.load('assets/dizzy-walk-left-4.png').convert()
-    WALKING_L5 = pg.image.load('assets/dizzy-walk-left-5.png').convert()
-    WALKING_L6 = pg.image.load('assets/dizzy-walk-left-6.png').convert()
-    WALKING_L7 = pg.image.load('assets/dizzy-walk-left-7.png').convert()
+    WALKING_FRAMES_LEFT = [
+        pg.image.load('assets/dizzy-walk-left-1.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-2.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-3.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-4.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-5.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-6.png').convert(),
+        pg.image.load('assets/dizzy-walk-left-7.png').convert()
+    ]
 
-    WALKING_R1 = pg.image.load('assets/dizzy-walk-right-1.png').convert()
-    WALKING_R2 = pg.image.load('assets/dizzy-walk-right-2.png').convert()
-    WALKING_R3 = pg.image.load('assets/dizzy-walk-right-3.png').convert()
-    WALKING_R4 = pg.image.load('assets/dizzy-walk-right-4.png').convert()
-    WALKING_R5 = pg.image.load('assets/dizzy-walk-right-5.png').convert()
-    WALKING_R6 = pg.image.load('assets/dizzy-walk-right-6.png').convert()
-    WALKING_R7 = pg.image.load('assets/dizzy-walk-right-7.png').convert()
+    WALKING_FRAMES_RIGHT = [
+        pg.image.load('assets/dizzy-walk-right-1.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-2.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-3.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-4.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-5.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-6.png').convert(),
+        pg.image.load('assets/dizzy-walk-right-7.png').convert()
+    ]
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.surface = self.STILL_1
+        self.surface = self.STILL_FRAMES[0]
         self.rect = self.surface.get_rect(center=(self.x, self.y))
         self.animation_index = 0
 
-    def animate_walk(self, left, right):
-        if self.animation_index >= 7:
-            self.animation_index = 0
-        
+    def animate_walk(self, left, right):        
         if left:
-            frames = [
-            self.WALKING_L1, self.WALKING_L2, self.WALKING_L3,
-            self.WALKING_L4, self.WALKING_L5, self.WALKING_L6, 
-            self.WALKING_L7
-            ]
+            frames = self.WALKING_FRAMES_LEFT
 
         elif right:
-            frames = [
-            self.WALKING_R1, self.WALKING_R2, self.WALKING_R3,
-            self.WALKING_R4, self.WALKING_R5, self.WALKING_R6, 
-            self.WALKING_R7
-            ]
+            frames = self.WALKING_FRAMES_RIGHT
 
         else:
-            if self.animation_index >= 2:
-                self.animation_index = 0
-            frames = [self.STILL_1, self.STILL_2]
+            frames = self.STILL_FRAMES
+
+        if self.animation_index >= len(frames):
+            self.animation_index = 0
 
         self.surface = frames[self.animation_index]
         self.rect = self.surface.get_rect(center=(self.x, self.y))
